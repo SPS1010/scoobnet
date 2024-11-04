b0967333099b5e36b60fef15c42599f860c9f566
zeus <136583387+zeusssz@users.noreply.github.com>
2024-11-04 16:23:30 +0530

    Update postHunt-BE.py

--- betterend/backend/postHunt-BE.py
+++ betterend/backend/postHunt-BE.py
@@ -1,31 +1,46 @@
 import json
 import random
 import string
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 
-app=Flask(__name__)
-DB_FILE='hunts.json'
-def genid():return ''.join(random.choices(string.digits,k=10))
+app = Flask(__name__)
+DB_FILE = 'hunts.json'
+
+def genid():
+    return ''.join(random.choices(string.digits, k=10))
+
 def loaddb():
- try:
-  with open(DB_FILE,'r') as file:return json.load(file)
- except FileNotFoundError:return {}
+    try:
+        with open(DB_FILE, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
 
 def savedb(data):
- with open(DB_FILE,'w') as file:json.dump(data,file,indent=4)
+    with open(DB_FILE, 'w') as file:
+        json.dump(data, file, indent=4)
 
-@app.route('/create-post',methods=['POST'])
-def postPost():
- data=request.json
- hunt_id=genid()
- new_post={
-  "huntName":data.get("huntName","Unnamed Hunt"),
-  "huntOwner":data.get("huntOwner","Anonymous"),
-  "huntActive":data.get("huntActive","true"),
-  "huntDesc":data.get("huntDesc",""),
-  "huntBanner":data.get("huntBanner","")}
- db=loaddb();db[hunt_id]=new_post;savedb(db)
- return jsonify({"message":"Post created","hunt-id":hunt_id}),201
+@app.route('/create-hunt', methods=['POST'])
+def createHunt():
+    data = request.json
+    hunt_id = genid()
+    new_hunt = {
+        "huntName": data.get("huntName", "Unnamed Hunt"),
+        "huntOwner": data.get("huntOwner", "Anonymous"),
+        "huntActive": data.get("huntActive", True),
+        "huntDesc": data.get("huntDesc", ""),
+        "huntBanner": data.get("huntBanner", "")
+    }
+    db = loaddb()
+    db[hunt_id] = new_hunt
+    savedb(db)
+    return jsonify({"message": "Hunt created", "hunt-id": hunt_id}), 201
 
-if __name__=='__main__':
- app.run(debug=True)
+@app.route('/hunts', methods=['GET'])
+def getHunts():
+    db = loaddb()
+    hunts_list = [{"huntID": k, **v} for k, v in db.items()]
+    return jsonify({"hunts": hunts_list})
+
+if __name__ == '__main__':
+    app.run(debug=True)
